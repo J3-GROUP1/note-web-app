13f71cf8579c3be51f0beb8fa4d8c12699cb4fe3
John Idenyi <johnidenyi2001@gmail.com>
2025-04-28 14:58:53 +0100

    Added images

--- frontend/src/components/EmptyCard/EmptyCard.jsx
+++ frontend/src/components/EmptyCard/EmptyCard.jsx
@@ -1,11 +1,11 @@
 export default function EmptyCard({ imgSrc, message }) {
-    return (
-      <div className="flex flex-col items-center justify-center h-screen">
-        <img src={imgSrc} alt="No notes" className="w-60" />
-  
-        <p className="w-1/2 text-sm font-medium text-slate-700 text-center leading-7 mt-5">
-          {message}
-        </p>
-      </div>
-    );
-  }
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <img src={imgSrc} alt="No notes" className="w-60" />
+
+      <p className="w-1/2 text-sm font-medium text-slate-700 text-center leading-7 mt-5">
+        {message}
+      </p>
+    </div>
+  );
+}
